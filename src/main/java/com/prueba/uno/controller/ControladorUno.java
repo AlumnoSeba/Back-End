753dd64610c4ec05d74783d7d0a26fb66cb7e421
Seba <elagusnor@gmail.com>
2022-10-06 18:16:47 -0300

    cambios

--- src/main/java/com/prueba/uno/controller/ControladorUno.java
+++ src/main/java/com/prueba/uno/controller/ControladorUno.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins="https://portfolio-sebastian.web.app/portfolioweb")
+@CrossOrigin(origins="https://portfolio-sebastian.web.app")
 
 public class ControladorUno {
     
