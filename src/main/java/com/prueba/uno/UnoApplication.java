e048fce42cc347e0c5e14581ea04dac69fcecc4e
Seba <elagusnor@gmail.com>
2022-10-07 09:33:41 -0300

    make it better 2

--- src/main/java/com/prueba/uno/UnoApplication.java
+++ src/main/java/com/prueba/uno/UnoApplication.java
@@ -2,6 +2,9 @@ package com.prueba.uno;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UnoApplication {
@@ -9,5 +12,12 @@ public class UnoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UnoApplication.class, args);
 	}
-
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("https://portfolio-113b8.web.app/portfolioweb");
+			}
+		};
+	}
 }
